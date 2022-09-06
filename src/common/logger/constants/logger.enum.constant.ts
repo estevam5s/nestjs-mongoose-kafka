bff3f202094313f36b9f0b37c65e75f0e871ce01
Estevam <estevamsouzalaureth@gmail.com>
2022-09-05 23:58:05 -0300

    Initial commit

---
+++ src/common/logger/constants/logger.enum.constant.ts
@@ -0,0 +1,23 @@
+import { ENUM_KAFKA_REQUEST_METHOD } from '../../../kafka/constants/kafka.enum.constant';
+import { ENUM_REQUEST_METHOD } from '../../request/constants/request.enum.constant';
+
+export enum ENUM_LOGGER_LEVEL {
+    DEBUG = 'DEBUG',
+    INFO = 'INFO',
+    WARM = 'WARM',
+    FATAL = 'FATAL',
+}
+
+export enum ENUM_LOGGER_ACTION {
+    LOGIN = 'LOGIN',
+    TEST = 'TEST',
+}
+
+export const ENUM_LOGGER_METHOD = {
+    ...ENUM_KAFKA_REQUEST_METHOD,
+    ...ENUM_REQUEST_METHOD,
+};
+
+export type ENUM_LOGGER_METHOD =
+    | ENUM_KAFKA_REQUEST_METHOD
+    | ENUM_REQUEST_METHOD;
